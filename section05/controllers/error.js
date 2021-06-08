a3644c27fd27a1e2694f4998d72071d69c7204da
John Brooke <johnabrooke@gmail.com>
2021-06-08 00:06:37 -0600

    app enhancements

--- section05/controllers/error.js
+++ section05/controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404page = (req, res, next) => {
-    res.status(404).render('404Page', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404Page', { pageTitle: 'Page Not Found', path: '/404page' });
 };
